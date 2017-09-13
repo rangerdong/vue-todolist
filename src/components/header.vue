db62ff48a1276c8d5c1f4026237597c78489e43e
deandong <844199574@qq.com>
2017-09-14 01:12:29 +0800

    add component

---
+++ src/components/header.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <h1>{{ msg }}</h1>
+  <button v-on:click="clickHeader" >头部按钮</button>
+</div>
+</template>
+<script>
+export default {
+  data () {
+    msg: '点击之前'
+  },
+  methods: {
+    clickHeader() {
+      this.msg = '点击之后'
+    }
+  }
+}
+</script>
+
+<style scoped="">
+h1 {
+  font-weight: normal;
+}
+</style>
